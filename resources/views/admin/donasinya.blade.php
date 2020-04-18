7defaba4a75ca1f1b7e2c93531c98c936f1c7f04
muhammadtopan <taufanomt@gmail.com>
2020-04-18 07:56:44 +0700

    coba select2

--- resources/views/admin/donasinya.blade.php
+++ resources/views/admin/donasinya.blade.php
@@ -19,7 +19,7 @@
         <link rel="stylesheet" type="text/css" href="{{ url('backend/asset/css/plugins/fullcalendar.min.css') }}"/>
         <link href="{{ url('backend/asset/css/style.css') }}" rel="stylesheet">
         <!-- new plugin -->
-        <link rel="stylesheet" type="text/css" href="{{ url('backend/asset/select2/dist/css/select2.min.css') }}"/>
+        <link rel="stylesheet" type="text/css" href="{{ url('backend/asset/select/dist/css/select2.min.css') }}"/>
         <!-- end new plugin -->
         <!-- end: Css -->
 
@@ -333,7 +333,7 @@
 <script src="{{ url('backend/asset/js/plugins/jquery.datatables.min.js')}}"></script>
 <script src="{{ url('backend/asset/js/plugins/datatables.bootstrap.min.js')}}"></script>
 <script src="{{ url('backend/asset/js/plugins/jquery.nicescroll.js')}}"></script>
-<script src="{{ url('backend/asset/select2/dist/js/select2.min.js')}}"></script>
+<script src="{{ url('backend/asset/select/dist/js/select2.min.js')}}"></script>
 <script>
     $(document).ready(function() {
         $('.select2').select2({
